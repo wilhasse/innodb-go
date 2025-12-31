d2a7b32f5af8432cecb22276000385e40d441ca7
wilhasse <willianhasse@gmail.com>
2025-12-31 18:10:51 -0300

    IBGO-161: add rec skeleton constants

---
+++ rec/rec.go
@@ -0,0 +1,22 @@
+package rec
+
+// Record format constants mirrored from rem0rec.h.
+const (
+	RecInfoMinRecFlag  = 0x10
+	RecInfoDeletedFlag = 0x20
+
+	RecNOldExtraBytes = 6
+	RecNNewExtraBytes = 5
+
+	RecStatusOrdinary = 0
+	RecStatusNodePtr  = 1
+	RecStatusInfimum  = 2
+	RecStatusSupremum = 3
+
+	RecNewHeapNo      = 4
+	RecHeapNoShift    = 3
+	RecNodePtrSize    = 4
+	RecOffsHeaderSize = 2
+	RecOffsNormalSize = 100
+	RecOffsSmallSize  = 10
+)
---
+++ rec/rec_test.go
@@ -0,0 +1,39 @@
+package rec
+
+import "testing"
+
+func TestRecConstants(t *testing.T) {
+	if RecInfoMinRecFlag != 0x10 {
+		t.Fatalf("RecInfoMinRecFlag=%d", RecInfoMinRecFlag)
+	}
+	if RecInfoDeletedFlag != 0x20 {
+		t.Fatalf("RecInfoDeletedFlag=%d", RecInfoDeletedFlag)
+	}
+	if RecNOldExtraBytes != 6 {
+		t.Fatalf("RecNOldExtraBytes=%d", RecNOldExtraBytes)
+	}
+	if RecNNewExtraBytes != 5 {
+		t.Fatalf("RecNNewExtraBytes=%d", RecNNewExtraBytes)
+	}
+	if RecStatusOrdinary != 0 || RecStatusNodePtr != 1 || RecStatusInfimum != 2 || RecStatusSupremum != 3 {
+		t.Fatalf("rec status constants unexpected: %d %d %d %d", RecStatusOrdinary, RecStatusNodePtr, RecStatusInfimum, RecStatusSupremum)
+	}
+	if RecNewHeapNo != 4 {
+		t.Fatalf("RecNewHeapNo=%d", RecNewHeapNo)
+	}
+	if RecHeapNoShift != 3 {
+		t.Fatalf("RecHeapNoShift=%d", RecHeapNoShift)
+	}
+	if RecNodePtrSize != 4 {
+		t.Fatalf("RecNodePtrSize=%d", RecNodePtrSize)
+	}
+	if RecOffsHeaderSize != 2 {
+		t.Fatalf("RecOffsHeaderSize=%d", RecOffsHeaderSize)
+	}
+	if RecOffsNormalSize != 100 {
+		t.Fatalf("RecOffsNormalSize=%d", RecOffsNormalSize)
+	}
+	if RecOffsSmallSize != 10 {
+		t.Fatalf("RecOffsSmallSize=%d", RecOffsSmallSize)
+	}
+}
